0a291eeb8d0f3f5c8b37bd52e9a5d71f705018ca
Theawesometurtl <95330177+Theawesometurtl@users.noreply.github.com>
2025-04-25 18:38:33 -0700

    added matrix to graph class. Still gotta figure out which way matrix multiplication works with the library I have setup. Doesn't graph properly yet

--- src/DEMain.ts
+++ src/DEMain.ts
@@ -4,11 +4,13 @@ import { Rigidbody } from './rigidbody';
 import { rotateVector } from './rotateVector';
 import { Polygon } from './polygon';
 import { Circle } from './circle';
+import { Graph } from './graph';
 
 canvas.width = window.innerWidth/2 - 100
 canvas.height = window.innerHeight/2 - 10
 let canvas2: HTMLCanvasElement = document.getElementById("canvas2") as HTMLCanvasElement
 let ctx2: CanvasRenderingContext2D = canvas2.getContext("2d") as CanvasRenderingContext2D;
+export {ctx2, canvas2}
 
 let circleShape = new Circle()
 let circle: Rigidbody = new Rigidbody(circleShape, [100, 100], true, true, true)
@@ -16,6 +18,10 @@ let canvasStaticBottomShape = new Polygon([[0, canvas.height], [canvas.width, ca
 let canvasStaticBottom: Rigidbody = new Rigidbody(canvasStaticBottomShape,[canvas.width/2,canvas.height -10 + 5000],false,false, false)
 // let canvasStaticLeft: Rigidbody = new Rigidbody([[0, 0], [0, canvas.height], [-100, canvas.height], [-100, 0]],[0,0],false,false)
 // let canvasStaticRight: Rigidbody = new Rigidbody([[0, 0], [0, canvas.height], [-100, canvas.height], [-100, 0]],[canvas.width,0],false,false)
+
+let graph = new Graph()
+graph.draw()
+
 let interval = setInterval(() => main(), 1000/fps)
 interval
 function main() {
--- src/graph.ts
+++ src/graph.ts
@@ -1,39 +1,49 @@
 import { Matrix, Vector } from "ts-matrix";
-import { canvas, ctx } from "./globals";
+import { canvas2, ctx2 } from "./DEMain";
 
 
-export class graph {
+export class Graph {
     constructor() {
 
     }
     drawAxes() {
-        ctx.lineWidth = 5
-        ctx.strokeStyle = "black"
-        ctx.moveTo(canvas.width/2, 0)
-        ctx.lineTo(canvas.width/2, canvas.height)
-        ctx.moveTo(0, canvas.height/2)
-        ctx.lineTo(canvas.width, canvas.height/2)
-        ctx.stroke()
+        ctx2.lineWidth = 5
+        ctx2.strokeStyle = "black"
+        ctx2.moveTo(canvas2.width/2, 0)
+        ctx2.lineTo(canvas2.width/2, canvas2.height)
+        ctx2.moveTo(0, canvas2.height/2)
+        ctx2.lineTo(canvas2.width, canvas2.height/2)
+        ctx2.stroke()
     }
     draw() {
-        ctx.clearRect(0, 0, canvas.height, canvas.width)
+        ctx2.clearRect(0, 0, canvas2.height, canvas2.width)
         this.drawAxes()
+        this.drawArrow(100, 100, 0)
     }
     plotPoint(x: number, y: number) {
-        ctx.fillStyle = "white"
-        ctx.fillRect(x, y, 10, 10)
+        ctx2.fillStyle = "white"
+        ctx2.fillRect(x, y, 10, 10)
     }
     drawLine(point1: number[], point2: number[]) {
-        ctx.lineWidth = 5
-        ctx.strokeStyle = "black"
-        ctx.moveTo(point1[0], point1[1])
-        ctx.lineTo(point2[0], point2[1])
-        ctx.stroke()
+        ctx2.lineWidth = 5
+        ctx2.strokeStyle = "black"
+        ctx2.moveTo(point1[0], point1[1])
+        ctx2.lineTo(point2[0], point2[1])
+        ctx2.stroke()
     }
-    drawArrow(x: number, y: number, matrix: Matrix) {
+    drawArrow(x: number, y: number, rotation: number, xscalar: number = 1, yscalar: number = 1) {
+
+        let rotMatrix = new Matrix(2, 2, [[Math.cos(rotation), Math.sin(rotation)], [-Math.sin(rotation), Math.cos(rotation)]])
+        let scaleMatrix = new Matrix(2, 2, [[xscalar, 0], [0, yscalar]])
+        let compositionMatrix = rotMatrix.multiply(scaleMatrix)
         let arrow = new Matrix(2, 7,[[0, 0], [-5, -5], [5, -5], [2, -5], [2, -15], [-2, -15], [-2, -5]])
-        matrix.multiply(arrow)
-        
+        let finalArrow = compositionMatrix.multiply(arrow).values
+        ctx2.beginPath()
+        ctx2.moveTo(x, y)
+        for (let i = 0; i < arrow.values.length; i++) {
+            ctx2.lineTo(x + finalArrow[i][0], y + finalArrow[i][1])
+        }
+        ctx2.stroke()
     }
     slopeField() {
         
