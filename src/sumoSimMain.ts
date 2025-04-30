b3c90a801f54973559e85c70b145369998ed9512
Theawesometurtl <95330177+Theawesometurtl@users.noreply.github.com>
2025-04-30 13:37:30 -0700

    forgot to save my changes. Also the website doesn't work at all, weirdly enough

--- src/sumoSimMain.ts
+++ src/sumoSimMain.ts
@@ -7,12 +7,7 @@ import { Circle } from './circle';
 
 canvas.width = window.innerWidth - 100
 canvas.height = window.innerHeight - 10
-let square = new Polygon([[0, 0],[0,50],[50,50],[50,0]])
-let polygon: Rigidbody = new Rigidbody(square, [200, 100], false, true, false)
-let weirdShape = new Polygon([[0, 0],[0,50],[50,50],[50,0], [100, -100]])
-let weird: Rigidbody = new Rigidbody(weirdShape,[400, 400], true, true, false)
-let circleShape = new Circle()
-let circle: Rigidbody = new Rigidbody(circleShape, [100, 100], true, true, false)
+
 // let canvasStaticBottom: Rigidbody = new Rigidbody([[0, canvas.height], [canvas.width, canvas.height], [canvas.width, canvas.height+100], [0, canvas.height+100]],[0,0],false,false)
 // let canvasStaticLeft: Rigidbody = new Rigidbody([[0, 0], [0, canvas.height], [-100, canvas.height], [-100, 0]],[0,0],false,false)
 // let canvasStaticRight: Rigidbody = new Rigidbody([[0, 0], [0, canvas.height], [-100, canvas.height], [-100, 0]],[canvas.width,0],false,false)
@@ -20,13 +15,7 @@ let interval = setInterval(() => main(), 1000/fps)
 interval
 function main() {
   ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
-  polygon.update(1)
-  polygon.draw()
-  polygon.energyCalc()
-  weird.update(1)
-  weird.draw()
-  circle.update(1)
-  circle.draw()
+
   paint: Boolean;
   
   ctx.lineCap = 'round';
