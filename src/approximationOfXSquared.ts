925d301733cc1d70dbb17e571b05e4324db7f708
Theawesometurtl <95330177+Theawesometurtl@users.noreply.github.com>
2025-05-06 11:13:27 -0700

    I got confused with what a slope field should look like. Proper slope field now

--- src/approximationOfXSquared.ts
+++ src/approximationOfXSquared.ts
@@ -7,20 +7,10 @@ function thing(x: number[]): number[] {
     return [(x[0] **2)/50]
 }
 
-function dThing(t: number, y: number[]): number[] {
+function dStateVector(t: number, y: number[]): number[] {
     return [(t)/25]
 }
 
-function dStateVector( t: number,y: number[]) : number[] {
-    if (t != 0) {
-        return [(y[0]/25) * (t/Math.abs(t))]
-
-    }
-    else {
-        console.log("t == 0")
-        return [(y[0]/25)]
-    }
-}
 
 export function approximateXSquared(graph: Graph) { 
     graph.drawEquation(thing)
@@ -30,11 +20,9 @@ export function approximateXSquared(graph: Graph) {
     let backwardsEulerStateVectorPredictor: StateVectorPredictor = (y0: number[], len: number, t0: number, t1: number) => backwardsEuler(y0, len, t0, t1, dStateVector);
     let modifiedEulerStateVectorPredictor: StateVectorPredictor = (y0: number[], len: number, t0: number, t1: number) => modifiedEuler(y0, len, t0, t1, dStateVector);
     let rungeKutta4thOrderStateVectorPredictor: StateVectorPredictor = (y0: number[], len: number, t0: number, t1: number) => rungeKutta4thOrder(y0, len, t0, t1, dStateVector);
-    let rungeKutta4thOrderStateVectorPredictorCheat: StateVectorPredictor = (y0: number[], len: number, t0: number, t1: number) => rungeKutta4thOrder(y0, len, t0, t1, dThing);
     
     graph.drawODE(eulerStateVectorPredictor, "green")
     graph.drawODE(backwardsEulerStateVectorPredictor, "red")
     graph.drawODE(modifiedEulerStateVectorPredictor, "yellow")
     graph.drawODE(rungeKutta4thOrderStateVectorPredictor, "blue")
-    graph.drawODE(rungeKutta4thOrderStateVectorPredictorCheat, "orange")
 }
